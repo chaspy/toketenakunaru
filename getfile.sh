8947a0ca47e6690e1a57fb0a0be817b6a9e16d59
kondo takeshi <take.she12@gmail.com>
2017-01-27 00:57:53 +0900

    add more information

--- getfile.sh
+++ getfile.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 # check file exists
-if [ -e infolist ]; then
-  rm infolist
+if [ -e info/infolist ]; then
+  rm info/infolist
+fi
+if [ -e info/infoalllist ]; then
+  rm info/infoalllist
 fi
 
 # get early 3 files
 files=`ls info | sort -r | head -n 3`
+allfiles=`ls info | sort -r`
 
 # except extention(.html)
 for filepath in $files; do
-  basename $filepath .html >> infolist
+  basename $filepath .html >> info/infolist
 done
-
+for filepath in $allfiles; do
+  basename $filepath .html >> info/infoalllist
+done
