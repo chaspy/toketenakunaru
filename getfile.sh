f8d02c280e568ac76c96fbca983aae8d77b08185
kondo takeshi <take.she12@gmail.com>
2017-01-28 20:04:21 +0900

    refactor script

--- getfile.sh
+++ getfile.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 # check file exists
-if [ -e info/infolist ]; then
-  rm info/infolist
-fi
-if [ -e info/infoalllist ]; then
-  rm info/infoalllist
-fi
+for file in infolist infoalllist
+do
+  if [ -e info/$file ]; then
+    rm info/$file
+  fi
+done
 
 # get early 3 files
-files=`ls info | sort -r | head -n 3`
 allfiles=`ls info | sort -r`
 
-# except extention(.html)
-for filepath in $files; do
-  basename $filepath .html >> info/infolist
-done
-for filepath in $allfiles; do
-  basename $filepath .html >> info/infoalllist
+for file in infolist infoalllist
+do
+  i=0
+  for filepath in $allfiles; do
+    if [ $file = "infolist" ] && [ $i -eq 3 ]; then
+      break
+    fi
+    # except extention(.html)
+    basename $filepath .html >> info/$file
+    i=$(( i + 1 ))
+  done
 done
